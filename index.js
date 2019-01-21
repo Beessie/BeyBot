a6f28616a9c47d0d2a8be14c906806d4e14fd852
Sasipa Tumkhanon <sasipa@ucconnect.co.th>
2019-01-21 14:32:55 +0700

    events

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/webhook', (req, res) => {
         body: req.body
     }); //on web
     let body = req.body;
-    let events = body.events[0];
+    let events = body.events;
     let source = enents.source;
     let message = events.message;
     console.log('==> Body');
