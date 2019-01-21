c335369097936ff9276d10d79a50b62d8cf8ff44
Sasipa Tumkhanon <sasipa@ucconnect.co.th>
2019-01-21 13:03:21 +0700

    deploy heroku

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 //-------------require-------------//
 const express = require('express');
 const app = express();
-const PORT = process.env.POST || 6000
+var bodyParser = require('body-parser')
+const PORT = process.env.PORT || 6000
 //--------------------------------//
 
-app.post('/', (req, res) => {
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
     res.send({
         status: 'OK'
     }); //on web
     console.log('Get /');
 });
 
-
-
 app.post('/webhook', (req, res) => {
+    console.log('==> POST /webhook');
     res.send({
-        status: 'OK'
+        status: 'OK',
+        body: req.body
     }); //on web
-    console.log('POST /');
+    console.log('==> Body');
+    console.log(req.body);
 });
 
 app.listen(PORT, () => {
